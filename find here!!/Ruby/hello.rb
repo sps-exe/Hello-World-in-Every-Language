4ac961ada7a95ba6386c9b9d59d8658732297827
yats0x7 <kattayatharth@gmail.com>
2025-10-26 10:45:30 +0530

    Added Hello, World! in Ruby

---
+++ find here!!/Ruby/hello.rb
@@ -0,0 +1,7 @@
+# Simple Ruby program to print "Hello, World!"
+# To run this file:
+# 1. Make sure Ruby is installed.
+# 2. Open terminal in this folder.
+# 3. Run: Ruby hello.rb
+
+puts "Hello, World!"
